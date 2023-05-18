4e24731177159218bb32908a0ada3a85cd20f515
bugless <503044120@qq.com>
2023-05-19 00:22:39 +0800

    init

---
+++ app/src/main/java/org/huihui/videoedit/MainActivity.java
@@ -0,0 +1,36 @@
+package org.huihui.videoedit;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.huihui.videoedit.databinding.ActivityMainBinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'videoedit' library on application startup.
+    static {
+        System.loadLibrary("videoedit");
+    }
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Example of a call to a native method
+        TextView tv = binding.sampleText;
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'videoedit' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
